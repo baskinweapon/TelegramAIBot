79c6e039dea73c5c440cebc8dc583e155b950cc5
aleksandrbaskin <solohinalex1@gmail.com>
2023-02-07 17:12:11 +0200

    remove token

--- Scripts/Token.py
+++ Scripts/Token.py
@@ -1,5 +1,5 @@
 dataPath = "Data.json"
-token = "6122718696:AAGtj3M-tkQmAPgUNYP8Mx0D_5wR0MpB0H4"
+token = ""
 
 # key for open AI
-openai_api_key = "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"
+openai_api_key = ""
